88f2b2f38e1c6db1c2cf6a29d738c962d19df38d
MUHAMMAD RASIB <muhammadali20345@gmail.com>
2024-03-08 21:44:13 +0500

    Add project files.

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startbutton = new System.Windows.Forms.Button();
+            this.previousButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.ZoomInButton = new System.Windows.Forms.Button();
+            this.ZoomOutButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.fullScreenButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startbutton
+            // 
+            this.startbutton.Location = new System.Drawing.Point(37, 12);
+            this.startbutton.Name = "startbutton";
+            this.startbutton.Size = new System.Drawing.Size(75, 23);
+            this.startbutton.TabIndex = 0;
+            this.startbutton.Text = "Start";
+            this.startbutton.UseVisualStyleBackColor = true;
+            this.startbutton.Click += new System.EventHandler(this.startbutton_Click);
+            // 
+            // previousButton
+            // 
+            this.previousButton.Location = new System.Drawing.Point(118, 12);
+            this.previousButton.Name = "previousButton";
+            this.previousButton.Size = new System.Drawing.Size(82, 23);
+            this.previousButton.TabIndex = 1;
+            this.previousButton.Text = "<<";
+            this.previousButton.UseVisualStyleBackColor = true;
+            this.previousButton.Click += new System.EventHandler(this.previousButton_Click);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(206, 13);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(75, 23);
+            this.nextButton.TabIndex = 2;
+            this.nextButton.Text = ">>";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // ZoomInButton
+            // 
+            this.ZoomInButton.Location = new System.Drawing.Point(287, 12);
+            this.ZoomInButton.Name = "ZoomInButton";
+            this.ZoomInButton.Size = new System.Drawing.Size(75, 23);
+            this.ZoomInButton.TabIndex = 3;
+            this.ZoomInButton.Text = "Zoom In";
+            this.ZoomInButton.UseVisualStyleBackColor = true;
+            this.ZoomInButton.Click += new System.EventHandler(this.ZoomInButton_Click);
+            // 
+            // ZoomOutButton
+            // 
+            this.ZoomOutButton.Location = new System.Drawing.Point(368, 12);
+            this.ZoomOutButton.Name = "ZoomOutButton";
+            this.ZoomOutButton.Size = new System.Drawing.Size(97, 23);
+            this.ZoomOutButton.TabIndex = 4;
+            this.ZoomOutButton.Text = "Zoom Out";
+            this.ZoomOutButton.UseVisualStyleBackColor = true;
+            this.ZoomOutButton.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(84, 75);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(286, 183);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // fullScreenButton
+            // 
+            this.fullScreenButton.Location = new System.Drawing.Point(471, 13);
+            this.fullScreenButton.Name = "fullScreenButton";
+            this.fullScreenButton.Size = new System.Drawing.Size(102, 23);
+            this.fullScreenButton.TabIndex = 6;
+            this.fullScreenButton.Text = "Full Screen";
+            this.fullScreenButton.UseVisualStyleBackColor = true;
+            this.fullScreenButton.Click += new System.EventHandler(this.fullScreenButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp6.Properties.Resources._360_F_461470323_6TMQSkCCs9XQoTtyer8VCsFypxwRiDGU;
+            this.ClientSize = new System.Drawing.Size(703, 394);
+            this.Controls.Add(this.fullScreenButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ZoomOutButton);
+            this.Controls.Add(this.ZoomInButton);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.previousButton);
+            this.Controls.Add(this.startbutton);
+            this.Font = new System.Drawing.Font("Microsoft JhengHei UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Slides Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startbutton;
+        private System.Windows.Forms.Button previousButton;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Button ZoomInButton;
+        private System.Windows.Forms.Button ZoomOutButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button fullScreenButton;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        int index = 1;
+        bool fullScreen = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Visible = false;
+        }
+
+        // Start Button Functionality.
+        private void startbutton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = true;
+            if (index == 1)
+            {
+                LoadImageWithTransition(index);
+                index++;
+            }
+            else
+            {
+                MessageBox.Show("Click Next Button For More Slides");
+            }
+        }
+
+        // Loading Image with Fade-In Transition.
+        private async void LoadImageWithTransition(int index)
+        {
+            String folderPath = @"D:\picture\";
+            String imagePath = "Capture" + index + ".PNG";
+            String fullPath = Path.Combine(folderPath, imagePath);
+
+            if (File.Exists(fullPath))
+            {
+                Image newImage = Image.FromFile(fullPath);
+
+                // Fade out previous image if exists
+                if (pictureBox1.Image != null)
+                {
+                    for (double opacity = 1.0; opacity >= 0; opacity -= 0.05)
+                    {
+                        await Task.Delay(20);
+                        pictureBox1.Image = AdjustOpacity((Bitmap)pictureBox1.Image, opacity);
+                    }
+                }
+
+                // Display new image
+                pictureBox1.Image = newImage;
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                // Fade in new image
+                for (double opacity = 0.0; opacity <= 1.0; opacity += 0.05)
+                {
+                    await Task.Delay(20);
+                    pictureBox1.Image = AdjustOpacity((Bitmap)newImage, opacity);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Image Not Found !");
+            }
+        }
+
+        // Adjust Opacity of Image.
+        private Bitmap AdjustOpacity(Bitmap image, double opacity)
+        {
+            Bitmap bmp = new Bitmap(image.Width, image.Height);
+            Graphics graphics = Graphics.FromImage(bmp);
+            ColorMatrix colormatrix = new ColorMatrix();
+            colormatrix.Matrix33 = (float)opacity;
+            ImageAttributes imgAttribute = new ImageAttributes();
+            imgAttribute.SetColorMatrix(colormatrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
+            graphics.DrawImage(image, new Rectangle(0, 0, bmp.Width, bmp.Height), 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, imgAttribute);
+            graphics.Dispose();
+            return bmp;
+        }
+
+        // Next Button Functionality.
+        private void button3_Click(object sender, EventArgs e)
+        {
+            LoadImageWithTransition(index);
+            index++;
+        }
+
+        // Previous Button Functionality.
+        private void previousButton_Click(object sender, EventArgs e)
+        {
+            index--;
+            LoadImageWithTransition(index);
+        }
+
+        // Zoom In Functionality.
+        private void ZoomInButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Width = (int)(pictureBox1.Width * 1.1);
+            pictureBox1.Height = (int)(pictureBox1.Height * 1.1);
+        }
+
+        // Zoom Out Functionality.
+        private void button5_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Width = (int)(pictureBox1.Width / 1.1);
+            pictureBox1.Height = (int)(pictureBox1.Height / 1.1);
+        }
+
+        // Full Screen Functionality.
+        private void fullScreenButton_Click(object sender, EventArgs e)
+        {
+            if (fullScreen == false)
+            {
+                this.WindowState = FormWindowState.Maximized;
+                fullScreen = true;
+            }
+            else if (fullScreen == true)
+            {
+                this.WindowState = FormWindowState.Normal;
+                fullScreen = false;
+            }
+        }
+    }
+}
